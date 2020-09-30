27f0fb0a5dc20887d54a27130223ac8b8fc8ae30
lazy-af-7 <honey.batra007@icloud.com>
2020-09-30 23:15:43 +0530

    added api folders

---
+++ lib/Constants/keys.dart
@@ -0,0 +1,3 @@
+class api_keys {
+  static String movie_db_api = 'cd8e56c48d290ee186d2f57c021b8b0c';
+}
---
+++ lib/Services/api_manager.dart
@@ -0,0 +1,5 @@
+import 'package:http/http.dart' as http;
+
+class API_manager {
+  void get_trending() {}
+}
