b19e3673a424e4e20f64c4af8e90b33834ae7402
lazy-af-7 <honey.batra007@icloud.com>
2020-09-30 22:54:02 +0530

    appbar colors changed

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -13,8 +13,6 @@ import 'package:popcorn/Widgets/appbar.dart';
 //       fontSize: 40,
 //     ));
 
-
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -30,68 +28,80 @@ class Home extends StatefulWidget {
 //       fontSize: 40,
 //     ));
 
-
-
 class _HomeState extends State<Home> {
   @override
   Icon cus = Icon(
-  Icons.search,
-  size: 30.0,
-  color: Colors.white,
-);
-Widget search = Text("POPCORN",
-    style: TextStyle(
-      fontFamily: 'Bebas Neue',
-      fontSize: 40,
-    ));
+    Icons.search,
+    size: 30.0,
+    color: Hexcolor('#f8de8d'),
+  );
+  Widget search = Text("POPCORN",
+      style: TextStyle(
+        color: Hexcolor('#f8de8d'),
+        fontFamily: 'Bebas Neue',
+        fontSize: 40,
+      ));
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Hexcolor('#e23e57'),
-    leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-    title: Center(
-      child: search,
-    ),
-    elevation: 20.0,
-    actions: <Widget>[
-      IconButton(
-        padding: EdgeInsets.fromLTRB(0.0, 4.0, 18.0, 0.0),
-        // icon: Icon(
-        //   Icons.search,
-        //   size: 30.0,
-        //   color: Colors.white,
-        // ),
-        onPressed: () {
-          setState(() {
-          if(this.cus.icon == Icons.search){
-            this.cus = Icon(Icons.cancel);
-            this.search = TextField(
-              textInputAction: TextInputAction.go,
-              decoration: InputDecoration(
-                
-                hintText: "Search A Movie",
-              ),
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.0,
-              ),
-            );
-          }
-          else{
-            this.cus = Icon(Icons.search);
-            this.search = Text("POPCORN",
-    style: TextStyle(
-      fontFamily: 'Bebas Neue',
-      fontSize: 40,
-    ));          
-          }
-          });
-        },
-        icon: cus,
+        backgroundColor: Colors.lightBlue[600],
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Hexcolor('#f8de8d'),
+            ),
+            onPressed: () {}),
+        title: Center(
+          child: search,
+        ),
+        elevation: 20.0,
+        actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.fromLTRB(0.0, 4.0, 18.0, 0.0),
+            // icon: Icon(
+            //   Icons.search,
+            //   size: 30.0,
+            //   color: Colors.white,
+            // ),
+            onPressed: () {
+              setState(() {
+                if (this.cus.icon == Icons.search) {
+                  this.cus = Icon(
+                    Icons.cancel,
+                    color: Hexcolor('#f8de8d'),
+                  );
+                  this.search = TextField(
+                    textInputAction: TextInputAction.go,
+                    decoration: InputDecoration(
+                      hintText: "Search A Movie",
+                      hintStyle: TextStyle(
+                        color: Hexcolor('#f8de8d'),
+                      ),
+                    ),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  );
+                } else {
+                  this.cus = Icon(
+                    Icons.search,
+                    color: Hexcolor('#f8de8d'),
+                  );
+                  this.search = Text("POPCORN",
+                      style: TextStyle(
+                        fontFamily: 'Bebas Neue',
+                        fontSize: 40,
+                        color: Hexcolor('#f8de8d'),
+                      ));
+                }
+              });
+            },
+            icon: cus,
+          ),
+        ],
       ),
-    ],
-  ),
     );
   }
 }
